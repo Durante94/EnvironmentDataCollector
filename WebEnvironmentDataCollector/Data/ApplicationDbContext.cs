9b291f50a781b375b1e2481dbf7523b35df70ae3
Fabrizio Durante <fabrizio294.durante@outlook.com>
2021-09-30 17:30:33 +0200

    Aggiunto progetto web

---
+++ WebEnvironmentDataCollector/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebEnvironmentDataCollector.Models;
+
+namespace WebEnvironmentDataCollector.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ WebEnvironmentDataCollector/Models/AppUser.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebEnvironmentDataCollector.Models
+{
+    public class AppUser : IdentityUser
+    {
+        public AppUser() : base()
+        {
+            Active = true;
+        }
+
+        public AppUser(bool active) : base()
+        {
+            Active = active;
+        }
+
+        public AppUser(string userName) : base(userName)
+        {
+            Active = true;
+        }
+
+        public AppUser(string userName, bool active) : base(userName)
+        {
+            Active = active;
+        }
+
+
+        public bool Active { get; set; }
+
+        public string Motivo { get; set; }
+    }
+}
